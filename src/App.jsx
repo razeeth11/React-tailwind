4b6048c463987251e8363b2319fe13fdd0a63d47
razeeth11 <razeeth011@gmail.com>
2023-07-10 11:24:27 +0530

    Install-setUp

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <div className="App">
-      
+      <h1 className="text-center text-5xl font-light">Hello world!</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;

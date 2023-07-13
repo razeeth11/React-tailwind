6ab588dba614cf6d0c2beccb3bfa2b8cc925a463
razeeth11 <razeeth011@gmail.com>
2023-07-13 10:01:09 +0530

    Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,28 @@ function App() {
   return (
     <div className="App">
       <LeftPanel />
+      <Dashboard />
+    </div>
+  );
+}
+
+function Dashboard() {
+  return (
+    <div className="dashboard">
+      <div className="search-bar">
+        <div className="search">
+          <span className="material-symbols-outlined">search</span>
+          <input type="text" placeholder="Search file, person, service" />
+        </div>
+        <div className="acc-section">
+          <span className="material-symbols-outlined">mode_comment</span>
+          <span className="material-symbols-outlined">notifications</span>
+          <img
+            src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fHww&w=1000&q=80"
+            alt="account-image"
+          />
+        </div>
+      </div>
     </div>
   );
 }
